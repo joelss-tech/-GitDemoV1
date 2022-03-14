c0f9a48b5e85b567eba7fd73733e829d60033cb2
joe <joe@joee>
2022-03-14 11:44:17 +0300

    First commit

---
+++ GitDemoV1/src/com/demo/Test.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		if (hungry == "yes") {
+			System.out.println("eat something");
+		} else {
+			System.out.println("do some work");
+		}
+	}
+}
